ccd509a78ec3f9f826aff3fc6cfc7120d3bcb1b4
inkyu <87048860+grapestore@users.noreply.github.com>
2021-09-27 01:34:25 +0900

    Update tiny.c
    comment update

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -15,7 +15,8 @@ int main(int argc, char **argv)
   char hostname[MAXLINE], port[MAXLINE];
   socklen_t clientlen;
   struct sockaddr_storage clientaddr;
-
+  // ./tiny port  argc:2 argv[0]:./tiny argv[1]: port
+  // argument가 제대로 들어왔는지 check
   /* Check command line args */
   if (argc != 2)
   {
@@ -23,15 +24,18 @@ int main(int argc, char **argv)
     exit(1);
   }
 
+  // listen socket 생성 및 listen 상태 
   listenfd = Open_listenfd(argv[1]);
   while (1)
   {
     clientlen = sizeof(clientaddr);
+    //client가 보낸 connect 신호와 연결하여 connect socket 생성 후 clinetfd와 connfd를 연결해준다.
     connfd = Accept(listenfd, (SA *)&clientaddr,
                     &clientlen); // line:netp:tiny:accept
     Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE,
                 0);
     printf("Accepted connection from (%s, %s)\n", hostname, port);
+    
     doit(connfd);  // line:netp:tiny:doit
     Close(connfd); // line:netp:tiny:close
   }
@@ -44,11 +48,15 @@ void doit(int fd)
   char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
   char filename[MAXLINE], cgiargs[MAXLINE], filetype[MAXLINE];
   rio_t rio;
-  /* Read request line and headers */
+
+  // read 준비 및 클라이언트가 보낸 header를 읽는다
   Rio_readinitb(&rio, fd);
   Rio_readlineb(&rio, buf, MAXLINE);
   printf("Request headers:\n");
   printf("%s", buf);
+
+  // 클라이언트가 보낸 header 정보를 띄어쓰기를 기준으로 method uri version으로 각각 저장한다.
+  // request header : GET /url HTTP/1.1
   sscanf(buf, "%s %s %s", method, uri, version);
   if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD"))
   {
@@ -56,17 +64,24 @@ void doit(int fd)
                 "Tiny does not implement this method");
     return;
   }
+  
+  // 사실 request header는 한줄이상의 정보를 가지는데
+  // 때문에 protocol로 header 정보를 다 보냈다는의미로 마지막에 빈 줄을 보내줌으로써 header 요청이 끝난걸 나타낸다.
   read_requesthdrs(&rio);
+
+  // 들어온 uri를 통해 파일경로(html exe 등)와 매개변수를 받을 수 있다.
   is_static = parse_uri(uri, filename, cgiargs);
   
-  /* Parse URI from GET request */
+  // 클라이언트가 요청하는 file이 dir상에 존재하는지 check해준다.
   if (stat(filename, &sbuf) < 0)
   {
     clienterror(fd, filename, "404", "Not found",
                 "Tiny couldn’t find this file");
     return;
   }
-  /* Parse URI from HEAD request */
+
+  // 클라이언트가 HEAD 메소드를 보냈을때 작동하는 조건이며 이럴경우 body내용은 보내지지 않지만
+  // body 가 어떤 정보를 가지고있는지까지 tintyheader?정보까지는 보내준다.
   if (!strcasecmp(method, "HEAD"))
   {
     printf("check");
@@ -79,7 +94,9 @@ void doit(int fd)
     Rio_writen(fd, buf, strlen(buf));
     return;
   }
-  /* Parse URI from GET request */
+
+
+  // 클라이언트가 GET method를 보낼경우 정적 page와 동적 page를 나누어서 response해준다.
   if (is_static)
   { /* Serve static content */
     if (!(S_ISREG(sbuf.st_mode)) || !(S_IRUSR & sbuf.st_mode))
@@ -88,6 +105,7 @@ void doit(int fd)
                   "Tiny couldn’t read the file");
       return;
     }
+    // 정적 page인 경우 web server에서 바로 Return해 주며 끝난다.
     serve_static(fd, filename, sbuf.st_size);
   }
   else
@@ -160,7 +178,11 @@ void serve_static(int fd, char *filename, int filesize)
   printf("Response headers:\n");
   printf("%s", buf);
 
-  /* Send response body to client */
+  // 정적 page인경우 일단 파일 내용을 메모리로 올린다.
+  // 그 후 disk file은 더이상 필요없으니 close해주며
+  // memory에 맵핑된 data를 클라이언트한테 보내주고
+  // Munmap함수를 통해 메모리를 반환해준다.
+  // 때문에 mmap대신 malloc으로 munmap대신 free를  사용함으로써 대체가 가능하다.
   srcfd = Open(filename, O_RDONLY, 0);
   srcp = Mmap(0, filesize, PROT_READ, MAP_PRIVATE, srcfd, 0);
   Close(srcfd);
@@ -192,11 +214,19 @@ void serve_dynamic(int fd, char *filename, char *cgiargs)
   Rio_writen(fd, buf, strlen(buf));
   if (Fork() == 0)
   { /* Child */
-    /* Real server would set all CGI vars here */
+    // CGI 프로그래밍에게 매개변수 key value값을 전달해야하기때문에
+    // 환경변수를 이용하여 query_string라는 변수에 key value값을 저장해준다.
     setenv("QUERY_STRING", cgiargs, 1);
+    
+    // dup2함수를 이용하여 자식프로세스가 표준출력을 바로 클라이언트한테 할 수 있도록 소켓을 연결해준다.
     Dup2(fd, STDOUT_FILENO);              /* Redirect stdout to client */
+
+    //EXCE함수에 종류로 v는 vector e는 환경변수를 의미하는 함수이며
+    // 때문에 vector는 필요없으므로 빈리스트를 보내주며 , 환경변수 query_string을 쓸수있게 넘겨준다.
     Execve(filename, emptylist, environ); /* Run CGI program */
   }
+  // child process가 죽을때까지 아니면 좀비 프로세스가 되니까
+  // 반환하기를 기다린다.
   Wait(NULL); /* Parent waits for and reaps child */
 }
 
